89d86a85c248640fd0529fc30ba1505c8c302cc4
ZichenWei1 <130113122+ZichenWei1@users.noreply.github.com>
2023-05-21 23:42:55 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -66,17 +66,24 @@ function initializeServiceWorker() {
  */
 async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
-  // A1. TODO - Check local storage to see if there are any recipes.
-  //            If there are recipes, return them.
-  /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let buffer = new Array();
+
+
+  
+  
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+    const myPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("foo");
+      }, 300);
+    });
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
